39f6e2f5e02b9a1ca659387140f70525e6d43b95
AlexBoa45 <alexboadella02@gmail.com>
2025-09-22 11:00:38 +0200

    Update dron_topGeofence.py

--- crazyLink/modules_crazyflie/dron_topGeofence.py
+++ crazyLink/modules_crazyflie/dron_topGeofence.py
@@ -7,6 +7,21 @@ def _checkTopGeofence (self, alt):
     if self.checkMaxAlt == True:
         if alt >= self.maxAltGeofence:
                 logging.warning("[Dron] Dentro de zona de exclusion superior.")
+
+                callback, params = self.additional_data_top
+
+                if callback != None:
+                    if self.id == None:
+                        if params == None:
+                            callback()
+                        else:
+                            callback(params)
+                    else:
+                        if params == None:
+                            callback(self.id)
+                        else:
+                            callback(self.id, params)
+
                 return True
         else:
                 return False
@@ -24,9 +39,12 @@ def _moveTopGeofence (self):
                 time.sleep(1.5)
 
 # Primary function to start the top Geofence
-def startTopGeofence (self, maxAlt, processBreach = None):
+# The callback is activated when the drone tries to move outside this geofence.
+# Active movement of this geofence is not applied.
+def startTopGeofence (self, maxAlt,callback=None, params = None):
     self.maxAltGeofence = maxAlt
     self.checkMaxAlt = True
+    self.additional_data_top = [callback, params]
 
 # Function to stop the top geofence
 def stopTopGeofence (self):
