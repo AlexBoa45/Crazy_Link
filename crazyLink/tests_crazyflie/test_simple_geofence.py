bfce85d65228c1b68852502a2f4acaa54c927d14
AlexBoa45 <alexboadella02@gmail.com>
2025-09-22 10:58:41 +0200

    Update test_simple_geofence.py

--- crazyLink/tests_crazyflie/test_simple_geofence.py
+++ crazyLink/tests_crazyflie/test_simple_geofence.py
@@ -5,21 +5,22 @@ import math
 import logging
 import time
 
+
 global dron
 dron = Dron()
 dron.connect()
 print ('conectado')
 dron.arm()
-dron.setMoveSpeed(0.3)
+dron.setMoveSpeed(0.4)
 time.sleep(1)
 dron.takeOff (0.5)
 print ('ya he alcanzado los 0.5 metros')
 time.sleep(1)
-dron.move_distance ('Forward', 1.5)
+dron.move_distance('Forward', 1.5)
 print ('adelante 1.5 metros')
-time.sleep(1)
-dron.setSimpleScenario([1,5])
+time.sleep(2)
+dron.setSimpleScenario([1,1])
 print ('He creado una jaula de 1x5 m en base al centro, debería moverme ')
-time.sleep(5)
-dron.Land()
+time.sleep(7)
 print ('ya estoy en tierra')
+dron.Land()
